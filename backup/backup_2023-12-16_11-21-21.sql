8d082b3c5b5feb660b699c3cf22ebcd43de59bd9
Labib Azad <labibazad51@gmail.com>
2023-12-18 02:42:45 +0600

    Enhance database security measures DAILY DATABASE AUTOBACKUP
    - Implement proper data validation and input sanitization
    - Ensure password security with hashed storage
    - Validate and secure external dependencies, e.g., links in the 'medicine' table
    - Verify and enforce user access controls
    - Regularly update and secure database backups
    - Consider SSL/TLS encryption for database connections
    - Review and improve overall data consistency and integrity
    - Optimize query performance with proper indexing

---
+++ backup/backup_2023-12-16_11-21-21.sql
@@ -0,0 +1,238 @@
+-- MariaDB dump 10.19  Distrib 10.4.28-MariaDB, for osx10.10 (x86_64)
+--
+-- Host: localhost    Database: pres_db
+-- ------------------------------------------------------
+-- Server version	10.4.28-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `appointments`
+--
+
+DROP TABLE IF EXISTS `appointments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `appointments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `doctor` varchar(30) NOT NULL,
+  `department` varchar(30) NOT NULL,
+  `app_time` varchar(30) NOT NULL,
+  `app_date` varchar(30) NOT NULL,
+  `patient_name` varchar(30) NOT NULL,
+  `phone` varchar(30) NOT NULL,
+  `msg` varchar(60) NOT NULL,
+  `created_at` date NOT NULL DEFAULT current_timestamp(),
+  `dob` varchar(10) DEFAULT NULL,
+  `age` varchar(10) DEFAULT NULL,
+  `gender` varchar(10) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `appointments`
+--
+
+LOCK TABLES `appointments` WRITE;
+/*!40000 ALTER TABLE `appointments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `appointments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `departments`
+--
+
+DROP TABLE IF EXISTS `departments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `departments` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `department` varchar(30) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `departments`
+--
+
+LOCK TABLES `departments` WRITE;
+/*!40000 ALTER TABLE `departments` DISABLE KEYS */;
+INSERT INTO `departments` VALUES (1,'Cardiology'),(2,'Gastrology'),(3,'Neurology'),(4,'Dentist'),(5,'Gynocology'),(6,'Medicine');
+/*!40000 ALTER TABLE `departments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `doctors`
+--
+
+DROP TABLE IF EXISTS `doctors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `doctors` (
+  `ID` int(10) NOT NULL AUTO_INCREMENT,
+  `Full_Name` varchar(30) NOT NULL,
+  `Password` varchar(500) NOT NULL,
+  `DOB` varchar(10) DEFAULT NULL,
+  `Email` varchar(20) NOT NULL,
+  `Department` varchar(30) DEFAULT NULL,
+  `Designation` varchar(30) DEFAULT NULL,
+  `Gender` varchar(10) DEFAULT NULL,
+  `Phone` varchar(15) DEFAULT NULL,
+  `username` varchar(30) DEFAULT NULL,
+  `created_at` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `doctors`
+--
+
+LOCK TABLES `doctors` WRITE;
+/*!40000 ALTER TABLE `doctors` DISABLE KEYS */;
+INSERT INTO `doctors` VALUES (17,'Abdullah Al Sakib','a311d9acabf78ba343f8331fb46051e40561473b0ca99063868053f8b57175b1',NULL,'sakibdob@gmail.com',NULL,NULL,NULL,'01775332747','sakibdob@gmail.com','2023-12-12 21:00:05');
+/*!40000 ALTER TABLE `doctors` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `logininfo`
+--
+
+DROP TABLE IF EXISTS `logininfo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `logininfo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `device` varchar(100) NOT NULL,
+  `time` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `logininfo`
+--
+
+LOCK TABLES `logininfo` WRITE;
+/*!40000 ALTER TABLE `logininfo` DISABLE KEYS */;
+INSERT INTO `logininfo` VALUES (1,17,'Abdullah Al Sakib','sakibdob@gmail.com','Laptop','2023-12-15 18:31:05'),(2,17,'Abdullah Al Sakib','sakibdob@gmail.com','Laptop','2023-12-15 23:04:25'),(3,17,'Abdullah Al Sakib','sakibdob@gmail.com','Laptop','2023-12-16 01:25:23'),(4,17,'Abdullah Al Sakib','sakibdob@gmail.com','Laptop','2023-12-16 16:17:13');
+/*!40000 ALTER TABLE `logininfo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `medicine`
+--
+
+DROP TABLE IF EXISTS `medicine`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `medicine` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(30) NOT NULL,
+  `Generic` varchar(30) NOT NULL,
+  `Details` varchar(60) NOT NULL,
+  `company` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `medicine`
+--
+
+LOCK TABLES `medicine` WRITE;
+/*!40000 ALTER TABLE `medicine` DISABLE KEYS */;
+INSERT INTO `medicine` VALUES (4,'Napa','Paracetemol','Test Medicine','Beximco'),(6,'Seclo 500mg','Omeprazole','This is Demo Medicine to test','Square'),(11,'Flexi®','Aceclofenac','Osteoarthritis, rheumatoid arthritis and ankylosing spondyli','https://www.squarepharma.com.bd/index.php'),(12,'Camlosart™','Amlodipine + Olmesartan Medoxo','The usual starting dose is one tablet (5/20 mg) once daily. ','https://www.squarepharma.com.bd/index.php');
+/*!40000 ALTER TABLE `medicine` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `patients`
+--
+
+DROP TABLE IF EXISTS `patients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `patients` (
+  `ID` int(10) NOT NULL AUTO_INCREMENT,
+  `fullname` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `phone` varchar(30) NOT NULL,
+  `dob` varchar(30) NOT NULL,
+  `patient_ID` varchar(30) NOT NULL,
+  `gender` varchar(30) NOT NULL,
+  `age` varchar(30) NOT NULL,
+  `comment` varchar(60) NOT NULL,
+  `doctor_name` varchar(30) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `patients`
+--
+
+LOCK TABLES `patients` WRITE;
+/*!40000 ALTER TABLE `patients` DISABLE KEYS */;
+INSERT INTO `patients` VALUES (5,'Rownak','rownak@gmail.com','15154515','10/1/2000','4F1CS0FE','Female','22','Hello','Labib Azad'),(6,'Picchi','picchi@gmail.com','01780808091','12/12/12','6I5XTW5I','Female','10','Hello Dear Doctor, I need help right now can you fix a meeti','Labib Azad'),(7,'Rodela','rodela@gmail.com','01712851818','12/12/12','7RLCGPRD','Female','10','hello!!','Labib Azad');
+/*!40000 ALTER TABLE `patients` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `prescriptions`
+--
+
+DROP TABLE IF EXISTS `prescriptions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `prescriptions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `patient_name` varchar(30) NOT NULL,
+  `patient_id` varchar(30) NOT NULL,
+  `patient_age` varchar(30) NOT NULL,
+  `patient_dob` varchar(30) NOT NULL,
+  `Doctor_name` varchar(30) NOT NULL,
+  `medicine_name` varchar(30) NOT NULL,
+  `dose` varchar(30) NOT NULL,
+  `strength` varchar(30) NOT NULL,
+  `advice` varchar(30) NOT NULL,
+  `duration` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `prescriptions`
+--
+
+LOCK TABLES `prescriptions` WRITE;
+/*!40000 ALTER TABLE `prescriptions` DISABLE KEYS */;
+INSERT INTO `prescriptions` VALUES (1,'Pushpa','4sf5sdf','12','10/12/2000','Labib Azad','Seclo 500mg','1/1/1','','before meal','7days'),(2,'Pushpa','4sf5sdf','12','10/12/2000','Labib Azad','Flexi®','1','1','1','1'),(3,'Rodela','7RLCGPRD','10','12/12/12','Labib Azad','Flexi®','1','1','1','1day');
+/*!40000 ALTER TABLE `prescriptions` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-12-16 16:21:21
